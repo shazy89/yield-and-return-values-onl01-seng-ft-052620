03be42dd8912725dc212907a454a2b3c264cd36e
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-29 14:45:07 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -11,3 +11,14 @@ end
 
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
